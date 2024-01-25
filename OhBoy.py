f9ebe90c7f44010e6af9f12808961950efd66b86
emmanueltab <taboade@stthom.edu>
2024-01-25 14:57:20 -0600

    aa

---
+++ OhBoy.py
@@ -0,0 +1,49 @@
+def PLUS(a,b):
+    return a+b
+def MINUS(a,b):
+    return(a-b)
+def TIMES(a,b):
+    return(a*b)
+def DIVIDE(a,b):
+    return(a/b)
+def IDIV(a,b):
+    return (a//b)
+def REM(a,b):
+	if(b==0):
+		return(0)
+	else:
+		return(a%b)
+def NEG(a):
+    return -a
+
+
+def EQ(a,b):
+
+    return a==b
+def LT(a,b):
+    return a<b
+def GT(a,b):
+    return a>b
+def NE(a,b):
+    return a!=b
+def GE(a,b):
+    return a>=b
+def LE(a,b):
+    return a<=b
+def AND(a,b):
+    return a and b
+def OR(a,b):
+    return a or b
+def NOT(a):
+    return not a
+
+def INPUT(a):
+    print(a)
+    return float(input())
+
+def PRINT(a):
+    print(a)
+
+
+
+
